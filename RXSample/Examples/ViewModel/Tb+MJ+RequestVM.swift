3f1eb1496f2abf57d1e4c3b2b0cf1e3f28bf28a3
LZRight123 <350442340@QQ.com>
2019-09-09 11:35:14 +0800

    cancel当前请求 ？？？

--- RXSample/Examples/ViewModel/Tb+MJ+RequestVM.swift
+++ RXSample/Examples/ViewModel/Tb+MJ+RequestVM.swift
@@ -40,18 +40,6 @@ class FirstVM {
         endFooterRefresh = dataSource.asDriver()
             .map { $0.count == 5 ? EndMJFooter.endWithNoMoreData : EndMJFooter.end }
         
-        
-        input.otherRefresh.asDriver()
-            .flatMap{ _ in
-                FristService.originGetPlayList(1)
-            }
-            .drive(onNext: { [weak self] (arr) in
-                self?.dataSource.accept(arr)
-                self?.page = 1
-            })
-            .disposed(by: disposeBag)
-        
-        
         headerRequest
             .drive(onNext: { [weak self] (arr) in
                 self?.dataSource.accept(arr)
@@ -74,39 +62,54 @@ class FirstVM {
                 self.dataSource.accept(self.dataSource.value + arr)
             })
             .disposed(by: disposeBag)
+        
+        input.otherRefresh.asDriver()
+            .flatMapLatest{ _ in
+                FristService.originGetPlayList(1)
+            }
+            .drive(onNext: { [weak self] (arr) in
+                self?.dataSource.accept(arr)
+                self?.page = 1
+            })
+            .disposed(by: disposeBag)
     }
 }
 
 
 struct FristService {
-    static func getPlayList(_ index: Int) -> Driver<[SongModel]> {
-        return fmProvider.rx.request(.playlist(index: index))
-            .mapArray(SongModel.self, atKeyPath: "song")
-            .asDriver(onErrorJustReturn: [])
-    }
-    
     static func originGetPlayList(_ index: Int)  -> Driver<[SongModel]> {
         let str = "https://douban.fm/j/mine/playlist?type=n&channel=\(index)&from=mainsite"
         
         return Single.create {  single in
+            print("请求开始: \(index)")
             let cancellableToken = URLSession.shared.dataTask(with: URL(string: str)!) { (data, response, error) in
                 let response = Moya.Response(statusCode: 200, data: data ?? Data())
+                print("回调 \(index)")
                 if let arr = try? response.mapArray(SongModel.self, atKeyPath: "song") {
-                    print("成功")
+//                    print("成功")
                     single(.success(arr))
                 } else {
-                    print("失败")
+//                    print("失败")
                     single(.error(MoyaError.imageMapping(response)))
                 }
             }
             cancellableToken.resume()
             
             return Disposables.create {
+                print("上次的请求cancel了 \(index)")
                 cancellableToken.cancel()
             }
-        }.asDriver(onErrorJustReturn: [])
-        
-        
-        
+            }.asDriver(onErrorJustReturn: [])
     }
+    
+    
+    
+    
+    static func getPlayList(_ index: Int) -> Driver<[SongModel]> {
+        return fmProvider.rx.request(.playlist(index: index))
+            .mapArray(SongModel.self, atKeyPath: "song")
+            .asDriver(onErrorJustReturn: [])
+    }
+    
+    
 }
