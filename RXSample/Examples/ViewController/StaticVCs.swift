09ce0451c2867211ae5d590f124fdc1ff59b7e01
LZRight123 <350442340@QQ.com>
2019-10-25 17:19:16 +0800

    fix

--- RXSample/Examples/ViewController/StaticVCs.swift
+++ RXSample/Examples/ViewController/StaticVCs.swift
@@ -12,27 +12,33 @@ import RxSwift
 
 class StaticVCs: UIViewController {
     let disposeBag = DisposeBag()
-
+    
     let yqm = "邀请码帮助说明"
     
     private lazy var tableView = UITableView(style: .plain, backgroundColor: .beijinghui)
-           .then{
-               $0.regiseter(UITableViewCell.self) //快捷注册cell方法 莫认 identifier = classname
-           }
-
+        .then{
+            $0.regiseter(UITableViewCell.self) //快捷注册cell方法 莫认 identifier = classname
+    }
+    
     struct Model {
+        enum AnimationType {
+            case pop
+            case push
+        }
         let title: String
         let viewname: String
+        let type: AnimationType
     }
     
     lazy var dataSource = BehaviorRelay<[Model]>(value:[
-        Model(title: yqm, viewname: "yqmbzsm")
+        Model(title: "邀请码帮助说明_原生", viewname: "yqmbzsm", type: .pop),
+        Model(title: "邀请码帮助说明_Vue", viewname: "yqmbzsm_vue", type: .pop)
     ])
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.addSubview(tableView)
         
         tableView.snp.makeConstraints { $0.edges.equalToSuperview() }
@@ -42,33 +48,38 @@ class StaticVCs: UIViewController {
             .bind(to: tableView.rx.items(cellIdentifier: UITableViewCell.identifier, cellType: UITableViewCell.self))
             { (row, model, cell) in
                 cell.textLabel?.text = model.title
-            }
-            .disposed(by: disposeBag)
-               
+        }
+        .disposed(by: disposeBag)
+        
         tableView.rx.modelSelected(Model.self)
             .subscribe(onNext: { [unowned self] (model) in
                 self.pushTo(model)
             })
             .disposed(by: disposeBag)
-
+        
         
     }
     
     
     func pushTo(_ model: Model){
-        let nextVC = WebVC()
-        nextVC.resource = model.viewname
-        show(nextVC, sender: nil)
+        switch model.type {
+        case .pop:
+            let nextVC = WebVC.build(model.viewname)
+            present(nextVC, animated: true, completion: nil)
+        case .push:
+            break
+        }
+        
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- RXSample/Examples/ViewController/WebVC.swift
+++ RXSample/Examples/ViewController/WebVC.swift
@@ -8,40 +8,77 @@
 
 import UIKit
 import WebKit
+import WebViewJavascriptBridge
 
 class WebVC: UIViewController {
+    
+    var resource: String!
+    
+    fileprivate let disposeBag = DisposeBag()
 
-    var resource = "yqmbzsm"
-
-    private lazy var webView = WKWebView()
+    private lazy var contetView = UIView(color: .white)
         .then{
-            guard let htmlPath = Bundle.main.path(forResource: resource, ofType: "html") else { return }
-            
-            guard let apphtml = try? String(contentsOfFile: htmlPath, encoding: .utf8) else { return }
-            $0.loadHTMLString(apphtml, baseURL: Bundle.main.bundleURL)
-            $0.scrollView.showsVerticalScrollIndicator = false
+            $0.cornerRadius = 16
+            $0.masksToBounds = true
+    }
+    
+    private lazy var wkConfig = WKWebViewConfiguration()
+        .then{
+            let userContent = WKUserContentController()
+            let userInfo = ["name": "wb", "sex": "male", "phone": "12333434"]
+            for key in userInfo.keys {
+                let script = WKUserScript(source: "var \(key) = \"\(userInfo[key]!)\"", injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: true)
+                userContent.addUserScript(script)
+            }
+            $0.userContentController = userContent
+
         }
     
+    private lazy var webView = WKWebView()
+        .then{
+            
+            guard let htmlPath = Bundle.main.path(forResource: resource, ofType: "html") else {
+                assert(false, "resource is nil")
+                return
+            }
+            
+            guard let apphtml = try? String(contentsOfFile: htmlPath, encoding: .utf8) else { return }
+            
+            $0.cornerRadius = 14
+            $0.masksToBounds = true
+            $0.loadHTMLString(apphtml, baseURL: Bundle.main.bundleURL)
+            $0.scrollView.bounces = false
+            $0.scrollView.showsVerticalScrollIndicator = false
+    }
+    
+    
     private var bridge: WKWebViewJavascriptBridge!
-
+    
+    static func build(_ resource: String) -> WebVC {
+        let vc = WebVC()
+        vc.resource = resource
+        vc.setupTransitioningAnimate(AnimatorOC.popup)
+        return vc
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .random
+        
+    
         view.addSubview(webView)
-        
         webView.snp.makeConstraints{
-            $0.center.equalToSuperview()
-            $0.width.equalToSuperview().multipliedBy(0.9)
-            $0.height.equalToSuperview().multipliedBy(0.7)
+            $0.top.equalToSuperview().offset(71)
+            $0.height.equalTo(min(594, ScreenHeight - 71 * 2))
+            $0.left.equalToSuperview().offset(47)
+            $0.right.equalToSuperview().offset(-47)
         }
+    
         
+   
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "reload", style: .plain, target: self, action: #selector(reload))
         
         WKWebViewJavascriptBridge.enableLogging()
-        bridge = WKWebViewJavascriptBridge.build(for: webView)
-        
+        bridge = WKWebViewJavascriptBridge(for: webView)
         registerHandler()
     }
     
@@ -49,12 +86,17 @@ class WebVC: UIViewController {
         webView.reloadFromOrigin()
     }
     
-     private func registerHandler() {
-    //        typedef void (^WVJBResponseCallback)(id responseData);
-    //        typedef void (^WVJBHandler)(id data, WVJBResponseCallback responseCallback);
-            bridge.registerHandler("appChangeColor") { [weak self] (data, callback) in
-                self?.view.backgroundColor = .random
-            }
+    private func registerHandler() {
+        //        typedef void (^WVJBResponseCallback)(id responseData);
+        //        typedef void (^WVJBHandler)(id data, WVJBResponseCallback responseCallback);
+        bridge.registerHandler("appChangeColor") { [weak self] (data, callback) in
+            print("call me")
+            self?.view.backgroundColor = .random
         }
-
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
